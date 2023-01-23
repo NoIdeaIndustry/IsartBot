8af4d4a456d5501af248028f4ca16fe8bf4c1e7f
NoIdeaIndustry <noideaindustry@gmail.com>
2023-01-23 22:18:21 +0000

    :fire: Github duplicates??!!

--- isartbot/src/main/java/com/noideaindustry/isartbot/commands/utils/RSlashArgument.java
+++
@@ -1,6 +0,0 @@
-package com.noideaindustry.isartbot.commands.utils;
-
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-
-public record RSlashArgument(OptionType type, String name, String description, boolean required) {
-}
